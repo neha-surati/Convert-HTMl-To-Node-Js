675ac693d6263510f1b394e51e2aeb3795b2a181
Neha <nehasurati1605@gmail.com>
2024-05-10 11:13:37 +0530

    Done

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ const app=express();
 
 app.set("view engine","ejs");
 
-app.use(express.static(path.join(__dirname,'/public')));
+app.use(express.static(path.join(__dirname+'/public')));
 
 // const middleware=(req,res,next)=>{
 //     console.log("hello..");
@@ -22,11 +22,80 @@ app.use(express.static(path.join(__dirname,'/public')));
 //  app.use(middleware)
 
 app.get('/',(req,res)=>{
-    return res.render('index')
+    return res.render('layout')
 })
 
 app.get('/form-basic',(req,res)=>{
-    return res.render('form-basic')
+    return res.render('./pages/form-basic')
+})
+app.get('/form-wizard',(req,res)=>{
+    return res.render('./pages/form-wizard')
+})
+app.get('/charts',(req,res)=>{
+    return res.render('./pages/charts')
+})
+app.get('/widgets',(req,res)=>{
+    return res.render('./pages/widgets')
+})
+app.get('/tables',(req,res)=>{
+    return res.render('./pages/tables')
+})
+app.get('/grid',(req,res)=>{
+    return res.render('./pages/grid')
+})
+app.get('/pages-buttons',(req,res)=>{
+    return res.render('./pages/pages-buttons')
+})
+
+app.get('/icon-material',(req,res)=>{
+    return res.render('./pages/icon-material')
+})
+
+app.get('/icon-fontawesome',(req,res)=>{
+    return res.render('./pages/icon-fontawesome')
+})
+
+app.get('/pages-elements',(req,res)=>{
+    return res.render('./pages/pages-elements')
+})
+app.get('/index2',(req,res)=>{
+    return res.render('./pages/index2')
+})
+app.get('/pages-gallery',(req,res)=>{
+    return res.render('./pages/pages-gallery')
+})
+app.get('/pages-calendar',(req,res)=>{
+    return res.render('./pages/pages-calendar')
+})
+
+app.get('/pages-invoice',(req,res)=>{
+    return res.render('./pages/pages-invoice')
+})
+
+app.get('/pages-chat',(req,res)=>{
+    return res.render('./pages/pages-chat')
+})
+
+app.get('/authentication-login',(req,res)=>{
+    return res.render('./pages/authentication-login')
+})
+
+
+app.get('/authentication-register',(req,res)=>{
+    return res.render('./pages/authentication-register')
+})
+
+app.get('/error-403',(req,res)=>{
+    return res.render('./pages/error-403')
+})
+app.get('/error-404',(req,res)=>{
+    return res.render('./pages/error-404')
+})
+app.get('/error-405',(req,res)=>{
+    return res.render('./pages/error-405')
+})
+app.get('/error-500',(req,res)=>{
+    return res.render('./pages/error-500')
 })
 
 
